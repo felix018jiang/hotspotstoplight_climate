f02c0870cdf2fa2b7d89a9482de2f11b6fa5f341
nlebovits <nissim.lebovits@vanderbilt.edu>
2024-02-11 15:58:14 -0500

    cleanup

--- flood_mapping/data/src/script.py
+++ flood_mapping/data/src/script.py
@@ -4,4 +4,4 @@ from data_utils.process_all_data import process_flood_data
 cloud_project = 'hotspotstoplight'
 ee.Initialize(project = cloud_project)
 
-process_flood_data('Austria')
+process_flood_data('Nicaragua')
